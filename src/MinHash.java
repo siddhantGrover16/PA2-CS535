226106d62f2c90dc1a7bac26a05d571985a43f08
siddhantGrover16 <46168937+siddhantGrover16@users.noreply.github.com>
2020-10-18 09:27:48 -0500

    termDocm Matrix
    updated term doc matrix

--- src/MinHash.java
+++ src/MinHash.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MinHash {
 
@@ -24,6 +25,7 @@ public class MinHash {
         for(int i=0;i< docs.length;i++){
             docs[i]=files[i].getName();
         }
+
         for (int i =0; i< files.length;i++){
             Dterms[i] = new ArrayList<String>();
             try {
@@ -36,19 +38,15 @@ public class MinHash {
                         Terms.add(terms[j]);// adding term to set of all terms
                         Dterms[i].add(terms[j]);// adding term to set of terms in particular document i
                     }
-
                     //System.out.println(data);
                 }
                 myReader.close();
-
             } catch (FileNotFoundException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
 
         }
-
-
 
         for (int i = 0; i < Terms.size();i++) {
             if (Terms.get(i) == "the") {
@@ -58,14 +56,19 @@ public class MinHash {
                 Terms.remove(i);
             }
         }
-        
+        List<String> unionTerms = Terms.stream().distinct().collect(Collectors.toList());
 
-        Set<String> unionTerms = new HashSet<String>(Terms);// removes all duplicates from the arraylist of universal terms
+        //Set<String> unionTerms = new HashSet<String>(Terms);// removes all duplicates from the arraylist of universal terms
         numTerms=unionTerms.size();
 
+        for(int i=0;i< unionTerms.size();i++){// each row that
+            for(int j=0;j< docs.length;j++){// each column
+                int occurrencesOfTerm = Collections.frequency(Dterms[j], unionTerms.get(i));
+                tdMatrix[i][j] = occurrencesOfTerm;
+            }
         }
 
-
+    }
 
 
     public String[] allDocs(){
@@ -78,12 +81,22 @@ public class MinHash {
 
     public int[][] minHashMatrix(){
 
+
         return mhMatrix;
     }
     public int[][] termDocumenMatrix(){
 
+        for(int i = 0; i< tdMatrix.length; i++)
+        {
+            for(int j = 0; j<tdMatrix[0].length; j++)
+            {
+                System.out.print(tdMatrix[i][j]);
+            }
+            System.out.println();
+        }
         return tdMatrix;
     }
+
     public int numTerms(){
 
         return numTerms;
