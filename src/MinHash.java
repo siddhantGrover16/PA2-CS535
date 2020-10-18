36af7a245800615221aed4a133fe8148895ced85
siddhantGrover16 <46168937+siddhantGrover16@users.noreply.github.com>
2020-10-17 22:31:22 -0500

    Began with MinHash

--- src/MinHash.java
+++ src/MinHash.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.*;
 
 public class MinHash {
 
@@ -11,16 +12,67 @@ public class MinHash {
    String[] docs;
 
 
+
     public MinHash(String folder, int numPermutations){
 
+        File dir = new File("src/"+folder);
+        File [] files = dir.listFiles();
+        String docs[] = new String[files.length];
+        ArrayList<String> Terms = new ArrayList<String>();
+        ArrayList<String>[] Dterms = new ArrayList[files.length];
+
+        for(int i=0;i< docs.length;i++){
+            docs[i]=files[i].getName();
+        }
+        for (int i =0; i< files.length;i++){
+            Dterms[i] = new ArrayList<String>();
+            try {
+                File myObj = new File(files[i].getAbsolutePath());
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    String[] terms = data.split("[,|.$':; ]");
+                    for (int j=0; j< terms.length;j++){
+                        Terms.add(terms[j]);// adding term to set of all terms
+                        Dterms[i].add(terms[j]);// adding term to set of terms in particular document i
+                    }
+
+                    //System.out.println(data);
+                }
+                myReader.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+        }
 
 
 
-    }
+        for (int i = 0; i < Terms.size();i++) {
+            if (Terms.get(i) == "the") {
+                Terms.remove(i);
+            }
+            if (Terms.get(i).length() <= 2) {
+                Terms.remove(i);
+            }
+        }
+        
+
+        Set<String> unionTerms = new HashSet<String>(Terms);// removes all duplicates from the arraylist of universal terms
+        numTerms=unionTerms.size();
+
+        }
+
+
 
 
     public String[] allDocs(){
 
+        for(int i=0 ; i< docs.length;i++){
+            System.out.println(docs[i]);
+        }
         return docs;
     }
 
---
+++ src/RoughTest.java
@@ -0,0 +1,5 @@
+public class RoughTest {
+    public static void main(String[] args) {
+
+    }
+}
