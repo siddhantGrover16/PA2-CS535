750a894521535802c3c93ac2fcae0115db09df3d
ibrotutic <tutici@iastate.edu>
2020-10-18 13:56:27 -0500

    Update test, fix bug

--- src/MinHash.java
+++ src/MinHash.java
@@ -23,12 +23,11 @@ public class MinHash {
         if (files != null) {
             for (File file : files) {
                 try (Stream<String> stream = Files.lines(file.toPath())) {
-                    stream.map(line -> {
-                        ArrayList<String> terms = (ArrayList<String>) Arrays.asList(line.split("[,|.$':; ]"));
+                    List<String> processedTerms = stream.map(line -> {
+                        List<String> terms = Arrays.asList(line.split("[,|.$':; ]"));
                         return terms.stream().map(String::toLowerCase).filter(term -> term.length() > 2 && !term.equalsIgnoreCase("the")).collect(Collectors.toList());
-                    }).forEach(processedTerms -> {
-                        termDocumentMatrix.indexTerms(file, processedTerms);
-                    });
+                    }).flatMap(List::stream).collect(Collectors.toList());
+                    termDocumentMatrix.indexTerms(file, processedTerms);
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
--- src/RoughTest.java
+++ src/RoughTest.java
@@ -1,5 +1,5 @@
 public class RoughTest {
     public static void main(String[] args) {
-
+        MinHash minHash = new MinHash("TestFiles", 0);
     }
 }
