367b04aa1c250f47daadd1ae06f571dd9b62aba9
siddhantGrover16 <46168937+siddhantGrover16@users.noreply.github.com>
2020-10-21 11:46:31 -0500

    minHash similiarity?
    SO i think this works,. although i spoke to the TA and term doc probably has to be no-binary

--- src/MinHashSimilarities.java
+++ src/MinHashSimilarities.java
@@ -1,18 +1,96 @@
+import java.util.LinkedHashSet;
+import java.util.List;
+
 public class MinHashSimilarities {
+    int[][] tdMatrix;
+    int[][] mhMatrix;
+    LinkedHashSet<String> allTerms;
+    private List<String> indexedDocuments;
+    MinHash minhash;
+    double eJacSim;
+    double aJacSim;
+    int [] mhSig;
+    int numPerm;
+    int intersect = 0;
+    int union = 0;
+    int sameMH;
 
-    public MinHashSimilarities(String folder, int numPermutations){
 
+    public MinHashSimilarities(String folder, int numPermutations) {
+        this.numPerm=numPermutations;
 
+        minhash = new MinHash(folder, numPermutations);
+        tdMatrix = minhash.termDocumentMatrix.getIntMatrix();
+        mhMatrix = minhash.minHashMatrix();
+        indexedDocuments = minhash.termDocumentMatrix.getDocuments();
     }
 
-    public float exactJaccard(String file1,String file2){
-        return 0;
+    public double exactJaccard(String file1, String file2) {
+       // indexedDocuments = minhash.termDocumentMatrix.getDocuments();
+        int d1 = indexedDocuments.indexOf(file1);
+        int d2 = indexedDocuments.indexOf(file2);
+        int intersect = 0;
+        int union = 0;
+        int x_a;
+        int x_b;
+
+        for (int i = 0; i < tdMatrix.length; i++) {// goes through each term  in td matrix
+            x_a = tdMatrix[i][d1];// frequency that term x appaears in d1 through term doc matrix
+            x_b = tdMatrix[i][d2];// frrquency that term x appears in d2 through term doc matrix
+            intersect += Math.min(x_a, x_b);// summation_ min(x_a,x_b)
+            union += Math.max(x_a, x_b);// summation_ max(x_a,x_b)
+        }
+//        System.out.println ("length is "+ tdMatrix.length);
+//        System.out.println("intersect is "+ intersect);
+//        System.out.println("union is "+ union);
+        eJacSim = (double)intersect / union;
+        return eJacSim;
     }
 
-    public int[] minHashSig(String filename){
+    public double approximateJaccard(String file1, String file2){
+       // indexedDocuments = minhash.termDocumentMatrix.getDocuments();
+        int d1 = indexedDocuments.indexOf(file1);
+       // System.out.println(" d1 is"+ d1);
+        int d2 = indexedDocuments.indexOf(file2);
+        //System.out.println(" d2 is"+ d2 );
 
-        return null;
+        for(int j =0 ; j< mhMatrix.length;j++){
+            if (mhMatrix[j][d1]==mhMatrix[j][d2]){
+               sameMH+=1;
+             //  System.out.println("SAME at row "+ j );
+            }
+        }
+       // System.out.println("num perm is "+ numPerm);
+        //System.out.println(" same is " +sameMH );
+        aJacSim = (double)sameMH/numPerm;
 
+        return aJacSim;
     }
 
+    public int[] minHashSig(String filename) {
+        int d1 = indexedDocuments.indexOf(filename);
+        mhSig = mhMatrix[d1];
+        return mhSig;
+
+    }
+    public int getTermCount(){
+        return minhash.numTerms();
+    }
+
+    public int getDocCount(){
+        return minhash.allDocs().size();
+    }
+
+    public int getNumPerm(){
+        return numPerm;
+    }
+
+    public int[][] getMhMatrix() {
+        return mhMatrix;
+    }
+
+    public int[][] getTdMatrix() {
+        return tdMatrix;
+    }
 }
+
--- src/RoughTest.java
+++ src/RoughTest.java
@@ -1,8 +1,60 @@
 public class RoughTest {
     public static void main(String[] args) {
-        MinHash minHash = new MinHash("TestFiles", 5);
-        TermDocumentMatrix termDocumentMatrix = minHash.getTermDocumentMatrix();
-        int[][] something = termDocumentMatrix.getIntMatrix();
-        int i = 0;
+
+        MinHashSimilarities minHash = new MinHashSimilarities("TestFiles",6);
+        int[][] tc =minHash.getTdMatrix();
+        int[][] mh = minHash.getMhMatrix();
+        int[] mhsig= minHash.minHashSig("baseball298.txt");
+
+        System.out.println("Term_Doc");
+        System.out.println("Number of Terms is "+ minHash.getTermCount());
+        System.out.println(" Number of docs is "+ minHash.getDocCount());
+
+        System.out.println(" ");
+
+
+
+        for( int i=0; i<5;i++){//
+            for(int j =0 ;j<tc[0].length;j++){
+                System.out.print(tc[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------------");
+        System.out.println("Min_Hash");
+        System.out.println(" Number of perms is "+ minHash.numPerm);
+        System.out.println("Number of docs is "+ minHash.getDocCount());
+
+        System.out.println(" ");
+
+        for( int i=0; i< mh.length;i++){
+            for(int j =0 ;j<mh[0].length;j++){
+                System.out.print(mh[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------");
+
+        double eJS = minHash.exactJaccard("space-0.txt","space-2.txt");
+        double aJS = minHash.approximateJaccard("space-0.txt","space-2.txt");
+
+        System.out.println("exact JacSim = "+ eJS);
+        System.out.println("approx JacSim = "+ aJS);
+        System.out.println("--------------------------------------------");
+
+        System.out.println("minHashSig of baseball298");
+
+        for (int i = 0; i < mhsig.length; i++) {
+            System.out.print ( mhsig[i] + " ");
+        }
+
+
+
+
+
+
+
+
+
     }
 }
