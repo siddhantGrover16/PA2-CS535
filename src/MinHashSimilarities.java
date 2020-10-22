3057f35fb5c887da8bd5d3e450a477c06689e9c2
siddhantGrover16 <46168937+siddhantGrover16@users.noreply.github.com>
2020-10-22 12:14:43 -0500

    Q3 complete ..

--- src/MinHashSimilarities.java
+++ src/MinHashSimilarities.java
@@ -26,45 +26,50 @@ public class MinHashSimilarities {
     }
 
     public double exactJaccard(String file1, String file2) {
-       // indexedDocuments = minhash.termDocumentMatrix.getDocuments();
+        // indexedDocuments = minhash.termDocumentMatrix.getDocuments();
         int d1 = indexedDocuments.indexOf(file1);
         int d2 = indexedDocuments.indexOf(file2);
+        eJacSim= computeEJac(d1,d2);
+
+        return eJacSim;
+    }
+    public double computeEJac(int docindex1, int docindex2){
+
         int intersect = 0;
         int union = 0;
         int x_a;
         int x_b;
 
         for (int i = 0; i < tdMatrix.length; i++) {// goes through each term  in td matrix
-            x_a = tdMatrix[i][d1];// frequency that term x appaears in d1 through term doc matrix
-            x_b = tdMatrix[i][d2];// frrquency that term x appears in d2 through term doc matrix
+            x_a = tdMatrix[i][docindex1];// frequency that term x appaears in d1 through term doc matrix
+            x_b = tdMatrix[i][docindex2];// frrquency that term x appears in d2 through term doc matrix
             intersect += Math.min(x_a, x_b);// summation_ min(x_a,x_b)
             union += Math.max(x_a, x_b);// summation_ max(x_a,x_b)
         }
 //        System.out.println ("length is "+ tdMatrix.length);
 //        System.out.println("intersect is "+ intersect);
 //        System.out.println("union is "+ union);
-        eJacSim = ((double)intersect) / union;
-        return eJacSim;
+
+        return ((double)intersect) / union;
     }
 
-    public double approximateJaccard(String file1, String file2){
-       // indexedDocuments = minhash.termDocumentMatrix.getDocuments();
+    public double approximateJaccard(String file1, String file2) {
         int d1 = indexedDocuments.indexOf(file1);
-       // System.out.println(" d1 is"+ d1);
         int d2 = indexedDocuments.indexOf(file2);
-        //System.out.println(" d2 is"+ d2 );
+        aJacSim = computeAJac(d1,d2);
+        return aJacSim;
+    }
+
+    public double computeAJac(int docindex1, int docindex2){
 
         for(int j =0 ; j< mhMatrix.length;j++){
-            if (mhMatrix[j][d1]==mhMatrix[j][d2]){
+            if (mhMatrix[j][docindex1]==mhMatrix[j][docindex2]){
                sameMH+=1;
              //  System.out.println("SAME at row "+ j );
             }
         }
-       // System.out.println("num perm is "+ numPerm);
-        //System.out.println(" same is " +sameMH );
-        aJacSim = ((double)sameMH)/numPerm;
 
-        return aJacSim;
+        return ((double)sameMH)/numPerm;
     }
 
     public int[] minHashSig(String filename) {
---
+++ src/MinHashTime.java
@@ -0,0 +1,63 @@
+import  java.lang.System.*;
+
+public class MinHashTime {
+    String folder;
+    int numPerm;
+  //  Pair timerPair;
+    MinHashSimilarities mhs;
+    /*
+    This class tests whether it is faster to estimate Jaccard Similarities using MinHash matrix. This
+class has a method named timer that
+ Gets name of a folder, number of permutations to be used as parameters, and creates an
+instance of MinHashSimilarities. Report the the time taken to construct an instance of
+MinHashSimilarities.
+ For every pair of les in the folder compute the exact Jaccard Similarity ; Report the time
+taken (in seconds) for this task.
+ Compute the MinHashMatrix and use this matrix to estimate Jaccard Similarity of every pair
+of documents in the collection. Report the time taken for this task.
+
+System.currentTimeinMillis()or by using  System.nanoTime().
+
+
+     */
+
+    public MinHashTime(String folder,int numPerm){
+        this.folder=folder;
+        this.numPerm =numPerm;
+        timer(folder,numPerm);
+    }
+
+    public void timer(String folder,int numPerm){
+       long starttime = System.currentTimeMillis();
+        mhs=new MinHashSimilarities(folder,numPerm);
+        long endtime = System.currentTimeMillis();
+         long timetocreate=endtime-starttime;
+        System.out.print("Time taken to create instance "+ timetocreate/1000);
+
+        starttime = System.currentTimeMillis();
+        for(int i=0; i<mhs.getDocCount()-1;i++){
+            for(int j=i+1;j<mhs.getDocCount();j++){
+                mhs.computeEJac(i,j);
+            }
+        }
+        endtime = System.currentTimeMillis();
+        long exactJtime= endtime-starttime;
+        System.out.print("Time taken to get Exact Jaccard for all pairs "+ exactJtime/1000);
+
+
+        starttime = System.currentTimeMillis();
+        for(int i=0; i<mhs.getDocCount()-1;i++){
+            for(int j=i+1;j<mhs.getDocCount();j++){
+                mhs.computeAJac(i,j);
+            }
+        }
+
+        endtime = System.currentTimeMillis();
+        long approxJtime= endtime-starttime;
+
+        System.out.print("Time taken to get Approximate for all pairs "+  approxJtime/1000);
+    }
+
+
+
+}
