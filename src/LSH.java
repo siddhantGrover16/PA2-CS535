9431c26c13e0bac048b5ef1ca57888894810a253
ibrotutic <tutici@iastate.edu>
2020-10-25 22:01:59 -0500

    Fix LSH

--- src/LSH.java
+++ src/LSH.java
@@ -5,46 +5,28 @@ public class LSH {
     private List<HashMap<Integer, List<String>>> lshStorage = new ArrayList<>();
     private List<List<String>> lshArrays = new ArrayList<>();
     private List<String> docNames;
-    private int[][] minHashMatrix;
     private int[][] transposedMinHash;
     private int rowsPerBand;
-    private long prime;
-    private Pair pair;
 
     public LSH(int[][] minHashMatrix, String[] docNames, int bands) {
         this.numBands = bands;
-        this.minHashMatrix = minHashMatrix;
         this.docNames = Arrays.asList(docNames);
         this.rowsPerBand = minHashMatrix.length / bands;
-        this.prime = PrimeNumberUtils.getPrimeLargerThan(minHashMatrix[0].length);
         this.transposedMinHash = transpose(minHashMatrix);
 
-        Random rand = new Random();
-        this.pair = new Pair(getRandomLong(this.prime, rand), getRandomLong(this.prime, rand));
         buildLSH();
     }
 
     private void buildLSH() {
         for (int i = 0; i < numBands; i++) {
             lshStorage.add(new HashMap<>());
-            lshArrays.add(new ArrayList<>());
         }
         for (int docIdx = 0; docIdx < docNames.size(); docIdx++) {
             for (int bandIdx = 0; bandIdx < numBands; bandIdx++) {
                 int hashCode = Arrays.hashCode(Arrays.copyOfRange(transposedMinHash[docIdx], rowsPerBand * bandIdx, rowsPerBand * bandIdx + rowsPerBand));
-                int hashIndex = getHash(hashCode);
-                lshStorage.get(hashIndex).computeIfAbsent(hashIndex, s -> new ArrayList<>()).add(docNames.get(docIdx));
-                lshArrays.get(hashIndex).add(docNames.get(docIdx));
+                lshStorage.get(bandIdx).computeIfAbsent(hashCode, s -> new ArrayList<>()).add(docNames.get(docIdx));
             }
         }
-    }
-
-    private long getRandomLong(long upperBound, Random rand) {
-        return Math.abs(rand.nextLong()) % upperBound;
-    }
-
-    private int getHash(int hashCode) {
-        return Math.round(Math.abs((pair.getA() * hashCode + pair.getB()) % numBands));
     }
 
     public ArrayList<String> nearDuplicatesOf(String docName){
@@ -52,8 +34,8 @@ public class LSH {
 
         HashSet<String> similarDocsSet = new HashSet<>();
         for (int bandIdx = 0; bandIdx < numBands; bandIdx++) {
-            HashMap<Integer, List<String>> similarBands = lshStorage.get(docIndex);
-            int hashIndex = getHash(Arrays.hashCode(Arrays.copyOfRange(transposedMinHash[docIndex], rowsPerBand * bandIdx, rowsPerBand * bandIdx + rowsPerBand)));
+            HashMap<Integer, List<String>> similarBands = lshStorage.get(bandIdx);
+            int hashIndex = Arrays.hashCode(Arrays.copyOfRange(transposedMinHash[docIndex], rowsPerBand * bandIdx, rowsPerBand * bandIdx + rowsPerBand));
             List<String> similarDocs = similarBands.getOrDefault(hashIndex, new ArrayList<>());
             similarDocsSet.addAll(similarDocs);
         }
@@ -61,6 +43,7 @@ public class LSH {
         return new ArrayList<>(similarDocsSet);
     }
 
+    //Transpose a matrix for fast access to columns (min hash matrix has rows for terms and columns for doc ids)
     public static int[][] transpose(int arr[][]){
         int m = arr.length;
         int n = arr[0].length;
